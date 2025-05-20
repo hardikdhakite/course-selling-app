5210f49ebd3e4e055e184d76a0d7113ebd2f201d
Hardik Dhakite <hardikdhakite2002@gmail.com>
2025-05-20 20:35:09 +0530

    Step 6 - Added the Schema

---
+++ db.js
@@ -0,0 +1,46 @@
+
+import mongoose from "mongoose";
+console.log("connected to");
+mongoose.connect("mongodb+srv://hardikdhakite2002:JkYfAk0ZGKOLQNKl@cluster0.y6dykhp.mongodb.net/coursera-app");
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId,
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+export {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+};
