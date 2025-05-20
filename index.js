4380d95de4a17a6c6968fc5248d0274c343a65a0
Hardik Dhakite <hardikdhakite2002@gmail.com>
2025-05-20 18:23:08 +0530

    making endpoints

--- index.js
+++ index.js
@@ -2,5 +2,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
+
+app.post("/user/signup", function (req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+app.post("/user/signin", function (req, res) {
+    res.json({
+        message: "signin endpoint"
+    })
+})
+
+app.get("/user/purchases", function (req, res) {
+    res.json({
+        message: "my courses endpoint"
+    })
+})
+
+app.get("/courses", function (req, res) {
+    res.json({
+        message: "all courses endpoint"
+    })
+})
 
 app.listen(3000);
