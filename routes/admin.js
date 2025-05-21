c20b73fdb9418fe68c65b8deca0aa3fb74ef93d9
Hardik Dhakite <hardikdhakite2002@gmail.com>
2025-05-21 17:06:38 +0530

    updated

--- routes/admin.js
+++ routes/admin.js
@@ -2,32 +2,38 @@ import { Router } from "express";
 import { adminModel } from "../db.js";
 
 const adminRouter = Router();
+app.use(express.json())
 
 adminRouter.post("/signup", function (req, res) {
+
     res.json({
         message: "signup endpoint"
     })
 })
 
 adminRouter.post("/signin", function (req, res) {
+
     res.json({
         message: "signin endpoint"
     })
 })
 
 adminRouter.post("/course", function (req, res) {
+
     res.json({
         message: "Add course"
     })
 })
 
 adminRouter.put("/course", function (req, res) {
+
     res.json({
         message: "Add course"
     })
 })
 
 adminRouter.get("/course/bulk", function (req, res) {
+    
     res.json({
         message: "Add course"
     })
