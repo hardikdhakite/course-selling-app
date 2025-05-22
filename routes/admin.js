2b817e23615be723354a7e1f7c619549d20c32dc
Hardik Dhakite <hardikdhakite2002@gmail.com>
2025-05-23 01:17:11 +0530

    Step 9 - add auth endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,49 @@
 import { Router } from "express";
 import { adminModel } from "../db.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const adminRouter = Router();
 
-adminRouter.post("/signup", function (req, res) {
+adminRouter.post("/signup", async function (req, res) {
+    const { email, password, firstName, lastName } = req.body;
+
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
 
     res.json({
-        message: "signup endpoint"
+        message: "signup succeeded"
     })
 })
 
-adminRouter.post("/signin", function (req, res) {
+adminRouter.post("/signin", async function (req, res) {
+    const { email, password } = req.body;
 
-    res.json({
-        message: "signin endpoint"
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
     })
+
+    if (admin) {
+        const token = jwt.sign({
+            id: admin._id
+        }, process.env.JWT_SECRET_ADMIN);
+
+        res.json({
+            token: token
+        })
+    }
+    else {
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
 })
 
 adminRouter.post("/course", function (req, res) {
@@ -32,7 +61,7 @@ adminRouter.put("/course", function (req, res) {
 })
 
 adminRouter.get("/course/bulk", function (req, res) {
-    
+
     res.json({
         message: "Add course"
     })
